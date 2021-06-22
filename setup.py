e4013fb41fc23f43a4b730fe51b9e74672660dac
BoxRight <70821453+BoxRight@users.noreply.github.com>
2021-06-22 14:01:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+
 
 setuptools.setup(
     name="TensorLogic",
@@ -9,13 +8,7 @@ setuptools.setup(
     author="Jesus Sesma",
     author_email="jesus.sesma@hotmail.com",
     description="A small tensorlogic package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/BoxRight/TensorLogic",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
